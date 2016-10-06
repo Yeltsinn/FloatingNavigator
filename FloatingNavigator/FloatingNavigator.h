38da31333fde372a839465d6e884b953965a7a93
yeltsinlobato <yeltsin.lobato@gmail.com>
2016-10-06 17:14:19 -0300

    Initial Commit

---
+++ FloatingNavigator/FloatingNavigator.h
@@ -0,0 +1,19 @@
+//
+//  FloatingNavigator.h
+//  FloatingNavigator
+//
+//  Created by Yeltsin Suares Lobato Gama on 10/6/16.
+//  Copyright © 2016 Yeltsin Suares Lobato Gama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FloatingNavigator.
+FOUNDATION_EXPORT double FloatingNavigatorVersionNumber;
+
+//! Project version string for FloatingNavigator.
+FOUNDATION_EXPORT const unsigned char FloatingNavigatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatingNavigator/PublicHeader.h>
+
+
