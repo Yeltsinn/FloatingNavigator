2373c21527f2a200f8702b0e714a516ffe5b4cd5
yeltsinlobato <yeltsin.lobato@gmail.com>
2016-10-07 09:13:46 -0300

    Init repository and setup main class

---
+++ FloatingNavigator/FNViewController+TabViewsSetup.swift
@@ -0,0 +1,35 @@
+//
+//  FNViewController+TabViewsSetup.swift
+//  FloatingNavigator
+//
+//  Created by Yeltsin Suares Lobato Gama on 10/7/16.
+//  Copyright © 2016 Yeltsin Suares Lobato Gama. All rights reserved.
+//
+
+import UIKit
+
+class FNViewController_TabViewsSetup: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
