2ab3ee8251980dec80528b1afa03fbbd2ae84dac
yeltsinlobato <yeltsin.lobato@gmail.com>
2016-10-11 20:24:47 -0300

    Setup FloatingNavigator DataSource

---
+++ FloatingNavigator/Util.swift
@@ -0,0 +1,13 @@
+//
+//  Util.swift
+//  FloatingNavigator
+//
+//  Created by Yeltsin Suares Lobato Gama on 10/11/16.
+//  Copyright © 2016 Yeltsin Suares Lobato Gama. All rights reserved.
+//
+
+import UIKit
+
+class Util: NSObject {
+
+}
